fed9a04d7bbb2b1cc38b73d36f743069408af885
unknown <sasiharish573@gmail.com>
2025-03-06 23:16:34 +0530

    project completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
             <div className='size-9 rounded-lg bg-primary/10 flex items-center justify-center'>
               <MessageSquare className='w-5 h-5 text-primary' />
             </div>
-            <h1 className='w-5 font-bold'> MsG</h1>
+            <h1 className='w-5 left-0 font-bold'> MsG</h1>
             </Link>
           </div>
           <div className='flex items-center gap-2'>
